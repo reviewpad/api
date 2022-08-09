7b4d80128337c7a8a34377f7c94d046a1a676a4f
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-08-09 14:55:43 +0100

    feat: semantic client

---
+++ go/clients/semantic.go
@@ -0,0 +1,26 @@
+// Copyright (C) 2022 Explore.dev, Unipessoal Lda - All Rights Reserved
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package clients
+
+import (
+	"github.com/reviewpad/api/go/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewSemanticClient(endpoint string) (services.SemanticClient, error) {
+	defaultOptions := grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(419430400))
+	transportCredentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+
+	semanticConnection, err := grpc.Dial(endpoint, transportCredentials, defaultOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer semanticConnection.Close()
+
+	semanticClient := services.NewSemanticClient(semanticConnection)
+
+	return semanticClient, nil
+}
