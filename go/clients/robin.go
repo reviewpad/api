874294dd481340ca04b1df791f0cb12dd9e03fd8
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2023-03-10 17:05:30 +0000

    feat: add robin client

---
+++ go/clients/robin.go
@@ -0,0 +1,25 @@
+// Copyright (C) 2023 Explore.dev, Unipessoal Lda - All Rights Reserved
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package clients
+
+import (
+	"github.com/reviewpad/api/go/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewRobinClient(endpoint string) (services.RobinClient, *grpc.ClientConn, error) {
+	defaultOptions := grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(419430400))
+	transportCredentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+
+	robinConnection, err := grpc.Dial(endpoint, transportCredentials, defaultOptions)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	robinClient := services.NewRobinClient(robinConnection)
+
+	return robinClient, robinConnection, nil
+}
