fcfe09cfade0170325fd2972702f822f1479953c
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2023-05-10 13:17:59 +0100

    feat(robin): rpc chat on git host entities

--- go/services_mocks/robin_service.go
+++ go/services_mocks/robin_service.go
@@ -36,6 +36,26 @@ func (m *MockRobinClient) EXPECT() *MockRobinClientMockRecorder {
 	return m.recorder
 }
 
+// ChatInCodeHost mocks base method.
+func (m *MockRobinClient) ChatInCodeHost(arg0 context.Context, arg1 *services.ChatInCodeHostRequest, arg2 ...grpc.CallOption) (*services.ChatInCodeHostReply, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "ChatInCodeHost", varargs...)
+	ret0, _ := ret[0].(*services.ChatInCodeHostReply)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ChatInCodeHost indicates an expected call of ChatInCodeHost.
+func (mr *MockRobinClientMockRecorder) ChatInCodeHost(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ChatInCodeHost", reflect.TypeOf((*MockRobinClient)(nil).ChatInCodeHost), varargs...)
+}
+
 // Explain mocks base method.
 func (m *MockRobinClient) Explain(arg0 context.Context, arg1 *services.ExplainRequest, arg2 ...grpc.CallOption) (*services.ExplainReply, error) {
 	m.ctrl.T.Helper()
